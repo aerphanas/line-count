3209de748a0bcadd229afd571ade347b0cf0b33f
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-04-24 20:31:48 +0700

    upload from local
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

---
+++ app/Main.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import Data.Maybe
+import GHC.IO.Exception
+
+import System.Environment
+import System.IO.Error
+import System.FilePath
+import Control.Exception
+
+handler :: IOError -> IO ()
+handler e
+  | isDoesNotExistError e = putStrLn $ "Error : File " <> baseName <> extName <> " not Found"
+  | otherwise = ioError e
+  where baseName = (takeBaseName . fromJust . ioeGetFileName) e
+        extName = (takeExtension . fromJust . ioeGetFileName) e
+
+trys :: IO ()
+trys = getArgs                                     >>=
+       (readFile . head)                           >>= \file ->
+       putStrLn $ "The file has "                  <>
+                   (show . length . lines) file <>
+                   " lines!"
+
+main :: IO ()
+main = catch trys handler
